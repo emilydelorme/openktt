aa0da2ede42cbb89d5a612389b9995a750944d77
emilydelorme <emilydelormefr@gmail.com>
2020-05-09 01:16:02 +0200

    Main call for test (Really WIP)

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -2,24 +2,28 @@ package main
 
 import game.building.factory.RawFactory
 import game.building.market.NationalMarket
+import game.flow.BasicFlow
 import game.product.BasicProduct
 import game.product.Product
 import game.product.ProductType
 
+public  val productTable : MutableMap<ProductType, Product> = mutableMapOf()
+
 fun main(args : Array<String>) {
 
-    val productTable : MutableMap<ProductType, Product> = mutableMapOf()
+
     productTable[ProductType.COAL] = BasicProduct(ProductType.COAL, 4)
 
-    val coalFactory = RawFactory("Coal Mine", 4, 0.5, 128, ProductType.COAL)
-    val nationalMarket = NationalMarket("Tokyo Market", 0.25)
+    val coalFactory = RawFactory(3, ProductType.COAL, "Tokyo Coal Mine", 0.5, mutableMapOf())
+    val nationalMarket = NationalMarket("Japan Nationnal Market", 1.0, mutableMapOf())
+    val flowCoalMarket = BasicFlow(ProductType.COAL, 2, coalFactory, nationalMarket)
 
-    println("Simulating 100 Ticks")
-    for (i in 0..100) {
+    println("Simulating 50 Ticks")
+    for (i in 0..50) {
         coalFactory.tick()
         nationalMarket.tick()
+        flowCoalMarket.tick()
     }
     println(coalFactory)
-    println("Value produced: ${coalFactory.getStock() * productTable[coalFactory.getProductType()]!!.getPrice() }€")
-    println("Value produced: ${coalFactory.getStock() * productTable[coalFactory.getProductType()]!!.getPrice() }€")
+    println(nationalMarket)
 }
