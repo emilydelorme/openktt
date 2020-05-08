0cd866ee05f41651dd7eebbb51d197ba6bebd7fb
emilydelorme <emilydelormefr@gmail.com>
2020-05-08 23:34:37 +0200

    Add main function

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,5 +1,25 @@
 package main
 
+import game.building.factory.RawFactory
+import game.building.market.NationalMarket
+import game.product.BasicProduct
+import game.product.Product
+import game.product.ProductType
+
 fun main(args : Array<String>) {
-    println("Hello, World!")
+
+    val productTable : MutableMap<ProductType, Product> = mutableMapOf()
+    productTable[ProductType.COAL] = BasicProduct(ProductType.COAL, 4)
+
+    val coalFactory = RawFactory("Coal Mine", 4, 0.5, 128, ProductType.COAL)
+    val nationalMarket = NationalMarket("Tokyo Market", 0.25)
+
+    println("Simulating 100 Ticks")
+    for (i in 0..100) {
+        coalFactory.tick()
+        nationalMarket.tick()
+    }
+    println(coalFactory)
+    println("Value produced: ${coalFactory.getStock() * productTable[coalFactory.getProductType()]!!.getPrice() }€")
+    println("Value produced: ${coalFactory.getStock() * productTable[coalFactory.getProductType()]!!.getPrice() }€")
 }
