76fdcbc846f92f025f40c41d208b98b9c0b5d63d
emilydelorme <emilydelormefr@gmail.com>
2020-05-09 02:15:58 +0200

    Add basic planet simulation for testing and print json

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,11 +1,19 @@
 package main
 
+import com.beust.klaxon.Klaxon
 import game.building.factory.RawFactory
 import game.building.market.NationalMarket
+import game.country.BasicCountry
 import game.flow.BasicFlow
+import game.map.planet.BasicPlanet
+import game.map.planet.Planet
+import game.map.region.BasicRegion
 import game.product.BasicProduct
 import game.product.Product
 import game.product.ProductType
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 public  val productTable : MutableMap<ProductType, Product> = mutableMapOf()
 
@@ -13,8 +21,17 @@ fun main(args : Array<String>) {
 
     productTable[ProductType.COAL] = BasicProduct(ProductType.COAL, 4)
 
+    val japan = BasicCountry("Japan")
+    val earth = BasicPlanet(name="Earth", belongTo = japan)
+    val honshu = BasicRegion(name="Honshū", belongTo = japan)
+    earth.regions.add(honshu)
+
     val coalFactory = RawFactory(3, ProductType.COAL, "Tokyo Coal Mine", 0.5, mutableMapOf())
     val nationalMarket = NationalMarket("Japan Nationnal Market", 1.0, mutableMapOf())
+    honshu.building.add(coalFactory)
+    honshu.building.add(nationalMarket)
+
+
     val flowCoalMarket = BasicFlow(ProductType.COAL, 2, coalFactory, nationalMarket)
 
     println("Simulating 50 Ticks")
@@ -23,6 +40,5 @@ fun main(args : Array<String>) {
         nationalMarket.tick()
         flowCoalMarket.tick()
     }
-    println(coalFactory)
-    println(nationalMarket)
+    Files.writeString(Paths.get("simulationResult.json"), Klaxon().toJsonString(earth))
 }
